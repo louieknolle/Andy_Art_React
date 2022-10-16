0aa85900644ed628b9d44817040e3c945fde56e2
Louie Knolle <91914975+louieknolle@users.noreply.github.com>
2022-10-16 16:20:25 -0400

    add header directly to navbar comp, fix collapse menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,73 +1,76 @@
 import React from "react";
 import { NavLink } from 'react-router-dom'
-import Header from "./Header";
 
 function NavBar() {
   return (
     <>
-      <Header />
-      <nav className="nav">
-        <ul className="menu">
-          <li>
-            <NavLink
-              to='/' 
-              className={({isActive})=>(isActive ? 'link active' : 'link')}
-              >
-              Collection 1
-            </NavLink>
-          </li>
-          <li>
-          <NavLink
-              to='/collection2' 
-              className={({isActive})=>(isActive ? 'link active' : 'link')}
-              >
-              Collection 2
-            </NavLink>
-          </li>
-          <li>
-          <NavLink
-              to='/collection3' 
-              className={({isActive})=>(isActive ? 'link active' : 'link')}
-              >
-              Collection 3
-            </NavLink>
-          </li>
-          <li>
-          <NavLink
-              to='/collection4' 
-              className={({isActive})=>(isActive ? 'link active' : 'link')}
-              >
-              Collection 4
-            </NavLink>
-          </li>
-          <div className="nonArtLinks">
+      <header className="header" id="sideNav">
+        <h1><a href="https://www.instagram.com/turning_granular/?hl=en" className="logo">Andy Knolle</a></h1>
+        <input className="side-menu" type="checkbox" id="side-menu"/>
+        <label className="hamb" for="side-menu"><span className="hamb-line"></span></label>
+        <nav className="nav">
+          <ul className="menu">
             <li>
               <NavLink
-                to='/blog' 
+                to='/' 
                 className={({isActive})=>(isActive ? 'link active' : 'link')}
                 >
-                Blog
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to='/about' 
-                className={({isActive})=>(isActive ? 'link active' : 'link')}
-                >
-                About
+                Collection 1
               </NavLink>
             </li>
             <li>
             <NavLink
-                to='/contact' 
+                to='/collection2' 
                 className={({isActive})=>(isActive ? 'link active' : 'link')}
                 >
-                Contact
-              </NavLink>  
-            </li> 
-          </div>
-        </ul>
-      </nav>
+                Collection 2
+              </NavLink>
+            </li>
+            <li>
+            <NavLink
+                to='/collection3' 
+                className={({isActive})=>(isActive ? 'link active' : 'link')}
+                >
+                Collection 3
+              </NavLink>
+            </li>
+            <li>
+            <NavLink
+                to='/collection4' 
+                className={({isActive})=>(isActive ? 'link active' : 'link')}
+                >
+                Collection 4
+              </NavLink>
+            </li>
+            <div className="nonArtLinks">
+              <li>
+                <NavLink
+                  to='/blog' 
+                  className={({isActive})=>(isActive ? 'link active' : 'link')}
+                  >
+                  Blog
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to='/about' 
+                  className={({isActive})=>(isActive ? 'link active' : 'link')}
+                  >
+                  About
+                </NavLink>
+              </li>
+              <li>
+              <NavLink
+                  to='/contact' 
+                  className={({isActive})=>(isActive ? 'link active' : 'link')}
+                  >
+                  Contact
+                </NavLink>  
+              </li> 
+            </div>
+          </ul>
+        </nav>
+      </header>
     </>
   );
 }
