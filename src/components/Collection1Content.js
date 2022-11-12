428f7dd4b43b8529c67de09ba497727ef1860805
Louie Knolle <knollelw@gmail.com>
2022-11-12 08:21:02 -0500

    remove storage code

--- src/components/Collection1Content.js
+++ src/components/Collection1Content.js
@@ -1,26 +1,17 @@
-import React, { useRef, useEffect, useState } from "react";
-// import {
-//   ref,
-//   getDownloadURL,
-//   list,
-// } from "firebase/storage";
-import storage from "../firebase";
-// import collection1img1 from "../img/1Collection1.jpg";
-// import collection1img2 from "../img/2Collection1.jpg";
-// import collection1img3 from "../img/3Collection1.jpg";
-// import collection1img4 from "../img/4Collection1.jpg";
+import React, { useRef, useEffect } from "react";
+import collection1img1 from "../img/1Collection1.jpg";
+import collection1img2 from "../img/2Collection1.jpg";
+import collection1img3 from "../img/3Collection1.jpg";
+import collection1img4 from "../img/4Collection1.jpg";
 
 function Collection1Content() {
-  // const collectionOneImages = [
-  //   collection1img1,
-  //   collection1img2,
-  //   collection1img3,
-  //   collection1img4,
-  // ];
-  // const [imageUrls, setImageUrls] = useState([]);
+  const collectionOneImages = [
+    collection1img1,
+    collection1img2,
+    collection1img3,
+    collection1img4,
+  ];
   const scrollContainerRef = useRef(null);
-
-  const imagesListRef = ref(storage, "collection1/");
 
   useEffect(() => {
     const handleScrollEvent = ({ wheelDeltaY }) => {
@@ -34,16 +25,6 @@ function Collection1Content() {
     };
   }, []);
 
-  useEffect(() => {
-    list(imagesListRef, { maxResults: 4 }).then((response) => {
-      response.items.forEach((item) => {
-        getDownloadURL(item).then((url) => {
-          setImageUrls((prev) => [...prev, url]);
-        });
-      });
-    });
-  }, []);
-
   return (
     <>
       <div
@@ -51,9 +32,9 @@ function Collection1Content() {
         className="content"
         ref={scrollContainerRef}
       >
-        {imageUrls.map((url) => {
-        return <img src={url} key={url} alt='fine art screenprint' className='collectionImage' />;
-      })}
+        {collectionOneImages.map((img) => (
+          <img key={img} src={img} alt={img} className="collectionImage" />
+        ))}
       </div>
       <div className="caption">
         <p>This is collection 1, cool.</p>
