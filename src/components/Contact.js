1458d2c2a498cea42671bf29374c8a53fe9edf1d
Louie Knolle <knollelw@gmail.com>
2022-11-13 20:43:38 -0500

    possibly change to frontend email service

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,48 +1,43 @@
 import React, { useState } from "react";
 
+const FORM_ENDPOINT = ""; // TODO - fill on the later step
+
 const Contact = () => {
-
-  const [status, setStatus] = useState("Submit");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setStatus("Sending...");
-
-    const { name, email, message } = e.target.elements;
-
-    let details = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    };
-
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(details),
-    });
-    
-    setStatus("Submit");
-    let result = await response.json();
-    alert(result.status);
+  const [submitted, setSubmitted] = useState(false);
+  const handleSubmit = () => {
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 100);
   };
+
+  if (submitted) {
+    return (
+      <>
+        <h2>Thank you!</h2>
+        <div>We'll be in touch soon.</div>
+      </>
+    );
+  }
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form
+      action={FORM_ENDPOINT}
+      onSubmit={handleSubmit}
+      method="POST"
+      target="_blank"
+    >
       <div>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" required />
+        <input type="text" placeholder="Your name" name="name" required />
       </div>
       <div>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" required />
+        <input type="email" placeholder="Email" name="email" required />
       </div>
       <div>
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" required />
+        <textarea placeholder="Your message" name="message" required />
       </div>
-      <button type="submit">{status}</button>
+      <div>
+        <button type="submit"> Send a message </button>
+      </div>
     </form>
   );
 };
