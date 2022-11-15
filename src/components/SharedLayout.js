f75bab4b7e0361f3e7c4a4578fbc900dc40a7641
Louie Knolle <knollelw@gmail.com>
2022-11-14 23:37:23 -0500

    trying to fix cut off content

--- src/components/SharedLayout.js
+++ src/components/SharedLayout.js
@@ -1,5 +1,7 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faInstagram } from "@fortawesome/free-solid-svg-icons";
 
 const mystyle = {
   alignSelf: "center",
@@ -15,12 +17,13 @@ const SharedLayout = () => {
             Portfolio currently under development, please view recent work on my
             Instagram.
           </p>
+          {/* <FontAwesomeIcon icon={faInstagram} /> */}
           <a
             href="https://www.instagram.com/turning_granular/?hl=en"
             target="_blank"
             rel="noreferrer"
           >
-            <i class="fa-brands fa-instagram"></i> @turning_granular
+            <i className="fa-brands fa-instagram"></i> @turning_granular
           </a>
         </div>
         <Outlet />
