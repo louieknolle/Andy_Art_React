ca3c9568fa11b648b8fd160ac35c9686b701a91e
Louie Knolle <knollelw@gmail.com>
2022-11-22 21:47:31 -0500

    update about

--- src/components/About.js
+++ src/components/About.js
@@ -5,22 +5,22 @@ const About = () => {
     <section className="content" id="about">
       <article>
         Born on the wrong side of the tracks, Andy Knolle began his art career
-        with the blood of a roadkill deer. With no brushes nearby, he struck a
-        match on an old broomstick and painted with the smoldering broom straw.
-        His primary motivations in life are anarchy, subverting your perception
-        of reality and the Chicago Black Sox. He's not adverse to shedding a
-        tear when moved by the beauty of his own work. He's the shit and
-        everyone should know it.
+        inspired by the macabre beauty of a roadkill deer. With no brushes
+        nearby, he struck a match on an old broomstick and painted with the
+        smoldering broom straw. His primary motivations in life are anarchy,
+        subverting your perception of reality and the Chicago Black Sox. He's
+        not adverse to shedding a tear when moved by the beauty of his own work.
+        He's the shit and everyone should know it.
       </article>
       <br></br>
       <article>
         His primary media are old ass camera film, block screen prints and the
         tortured love letters of his youth. He can be found in Cincinnati either
-        working in studios, photography gigs, or doing ollies at the skate park.
-        If you see something you like, please don't hesitate to give him a ring.
-        The only thing he loves more than developing film in his bathroom is
-        making sincere connections with fans of his work and genuinely dope
-        individuals.
+        working in studios, classying up photography gigs, or doing ollies at
+        the skate park. If you see something you like, please don't hesitate to
+        give him a ring. The only thing he loves more than developing film in
+        his bathroom is making sincere connections with fans of his work and
+        genuinely dope individuals.
       </article>
     </section>
   );
