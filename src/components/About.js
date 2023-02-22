346e376b44d701b753dbae86d85f8a8b2f8e7aad
Louie Knolle <knollelw@gmail.com>
2023-02-21 23:31:28 -0500

    content for bio/cv page

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,278 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React from "react";
 
 const About = () => {
   return (
-    <section className="content" id="about">
-      <article>
-        Andy Knolle (he/him b. 1993) is an interdisciplinary artist currently
-        working in Cincinnati, OH. He received his BFA in Studio Art and a
-        Certificate in Critical Visions from the University of Cincinnati’s
-        College of Design, Architecture, Art, and Planning (DAAP). He has shown
-        nationally and internationally at Kunsthalle am Hamburger Platz, Berlin.
-        Andy works with a process-driven practice to unpack the ideas of noise,
-        art as technology, and our relation to communicative technologies.
-      </article>
-    </section>
+    <div className="content" id="about">
+      <section className="aboutLeftCol">
+        <div className="aboutPhoto">
+          <img
+            src="https://lh3.googleusercontent.com/xsHRV3CtMlyy9cJeONcmTZoAOuTpE25NO9-NIk0Iy5R2fsDIkgdTLaf-Izvg1FKb5I4_xMmvRxbyKlaE1WGHATM0JLr6kFJnsT5tH8K6gt8ydXPHOpaWxU_GDsZdP0XkjIcF1jcEn6CsnWFw_AJDAOJwi8oZbq9R_y9ScQCxeWe1pWNP4ocChXdtm1EVEoO4rbb6OlIO3yu2DIa1aJap7HMFIj-nNZsKpUBbxS8qYszrmy0ma8YtlHDTqbdlmEGARCDNhJg7EEcHvUFl21JLU517mObHgvXd1KmzKWVBThKNm_6-EQKUMMpSTcKnESkD32X-I0ITpZUN3YmIUk9ipl7bl6wLNG-9sNUBrDPyJ0kDcbviNttpUp5UC9IrUEHcxRTPpSDiiXDvRyeKdNB4TzTj0mkueHlTCsw9FvAiBDf9Kw2E3_bpvtfAlqVJOUYqgNex8BfaABn8Bj9_mK60Pm7cz-tIm4VQTGsW-tILPO5baxREcqkqJBk_8aVq2EI68JyGKBvwUEvDtSOZMuV7q_LKnG7KPFWT5bubGggG3zrBLqEIwxnBhzYg-JyPij1xuojV0NDdN3hF3bFo_TnvDKSH0fppLPbRyEnNCTY4pTEhWfyArOMka-Ii3xPtb708if1kPz_Hmdx_yygPdTfTWcjZRl9jvx-5b4c_QR3Jbww_FAa7Q5JnZ7e79hFc8gl8Dzgk0dejLFBQi6umcsDhSvm8MhnTo-YTwA6MDCJZHnOL6SUy2Y1oOxNQMbH49s7_uVN8iTDuQtI9woy2C5fQ8gUi40_hyKwPPXBIApnVwptRR_gdlOmQraBzJxgO1v6rFn02plJdX1oSHtd6cyYs0KOXamrqQPGtJgzaXuuhqGh3XXOMAOFaVW129hMaS79uBnzOaSkXkpqAmNj2S1ItrHVyCuKwvjIxKlD5lUfuuZcCv5GDU-i7fg73kLjhGs1JXBj1WZyr_l5z0_eYSRbLWEcZxpQKqJupGitMNnRs0y7gqACAOFbMVqb_=w881-h1288-no?authuser=1"
+            alt="Andy Knolle"
+          />
+        </div>
+        <article>
+          Andy Knolle (he/him b. 1993) is an interdisciplinary artist currently
+          working in Cincinnati, OH. He received his BFA in Studio Art and a
+          Certificate in Critical Visions from the University of Cincinnati’s
+          College of Design, Architecture, Art, and Planning (DAAP). He has
+          shown nationally and internationally at Kunsthalle am Hamburger Platz,
+          Berlin. Andy works with a process-driven practice to unpack the ideas
+          of noise, art as technology, and our relation to communicative
+          technologies.
+        </article>
+      </section>
+      <section>
+        <div className="aboutRightCol">
+          <h2>Education</h2>
+          <ul>
+            <li>2016</li>
+            <li>
+              University of Cincinnati, College of Design, Architecture, Art, &
+              Planning (DAAP), Cincinnati, OH
+            </li>
+            <li>BFA Studio Art, Certificate in Critical Visions</li>
+          </ul>
+          <h2>Solo Exhibitions</h2>
+          <ul>
+            <li className="flex-dir-row">
+              <div>2019</div>
+              <div className="some-margin-left">
+                <p>Sub_ Divisions, OTOT Studios (Cincinnati, OH)*</p>
+              </div>
+            </li>
+            <li className="flex-dir-row">
+              <div>2016</div>
+              <div className="some-margin-left">
+                <p>Noisy Relations, The 840 Gallery (Cincinnati, OH)</p>
+              </div>
+            </li>
+            <li className="flex-dir-row">
+              <div>2014</div>
+              <div className="some-margin-left">
+                <p>//:gL/i_zch\|**, Public Installation (Cincinnati, OH)*</p>
+              </div>
+            </li>
+          </ul>
+          <p>*As a Collaboration with Victoria Weird</p>
+          <h2>Selected Group Exhibitions</h2>
+          <ul>
+            <li className="flex-dir-row">
+              <div>2022</div>
+              <div className="some-margin-left">
+                <p>(Y)Our Community, Elevar Gallery (Cincinnati, OH)</p>
+              </div>
+            </li>
+            <li className="flex-dir-row">
+              <div></div>
+              <div className="some-margin-left">
+                <p>The Teachers Lounge, The Kennedy Gallery (Cincinnati, OH)</p>
+              </div>
+            </li>
+            <li className="flex-dir-row">
+              <div>2021</div>
+              <div className="some-margin-left">
+                <p>Films Not Dead, The Lodge (Dayton, KY)</p>
+              </div>
+            </li>
+            <li className="flex-dir-row">
+              <div>2019</div>
+              <div className="some-margin-left">
+                <p>Smelling Like Roses, Bunk Spot Gallery (Cincinnati, OH)</p>
+              </div>
+            </li>
+            <li className="flex-dir-row">
+              <div>2018</div>
+              <div className="some-margin-left">
+                <p>UNFRAMED, Pike Street Gallery (Covington, KY)</p>
+              </div>
+            </li>
+            <li className="flex-dir-row">
+              <div>2016</div>
+              <div className="some-margin-left">
+                <p>DAAP Works, The Meyers Gallery (Cincinnati, OH)</p>
+                <p>SOA All-Stars, The Reed Gallery (Cincinnati, OH)</p>
+              </div>
+            </li>
+            <li className="flex-dir-row">
+              <div>2015</div>
+              <div className="some-margin-left">
+                <p>
+                  Broken Dreamz/ Constructed Realities, Kunstalle am Hamburger
+                  Platz (Berlin DE)
+                </p>
+                <p>Morph 2.0, The 840 Gallery (Cincinnati, OH)</p>
+              </div>
+            </li>
+            <li className="flex-dir-row">
+              <div>2013</div>
+              <div className="some-margin-left">
+                <p>The Beloved Blobject, The 840 Gallery (Cincinnati, OH)</p>
+              </div>
+            </li>
+          </ul>
+          <h2>Performances</h2>
+          <ul>
+            <li className="flex-dir-row">
+              <div>2015</div>
+              <div className="some-margin-left">
+                <p>
+                  Sound Reflections: Berlin, Max Kade Center of German- American
+                  Studies (Cincinnati, OH)
+                </p>
+              </div>
+            </li>
+            <li className="flex-dir-row">
+              <div>2014</div>
+              <div className="some-margin-left">
+                <p>Cateyes’ Assorted Sounds, First Friday, (Cincinnati, OH)</p>
+                <p>
+                  Lush: MFA Exhibition (Guitar Player for Julie Anne Ward),
+                  Contemporary Arts Center (Cincinnati, OH)
+                </p>
+              </div>
+            </li>
+          </ul>
+          <h2>Publications</h2>
+          <ul>
+            <li class="flex-dir-row">
+              <div>2022</div>
+              <div class="some-margin-left">
+                <p>
+                  Eunoia, Psyop of the Year, Nefarious Industries (United
+                  States)
+                </p>
+              </div>
+            </li>
+            <li class="flex-dir-row">
+              <div>2021</div>
+              <div class="some-margin-left">
+                <p>
+                  Fourth Wife, Five Word and a Fake one, Culture Vacuum Records
+                  (Cincinnati, OH)
+                </p>
+                <p>
+                  Mockery, Racing Mind, Independent Release (Cincinnati, OH)
+                </p>
+              </div>
+            </li>
+            <li class="flex-dir-row">
+              <div>2020</div>
+              <div class="some-margin-left">
+                <p>
+                  Mockery, Headcannon, Independent Release (Cincinnati, OH)*
+                </p>
+              </div>
+            </li>
+            <li class="flex-dir-row">
+              <div>2019</div>
+              <div class="some-margin-left">
+                <p>
+                  Hissing Tiles, Boychoir, White Sepulcher Records (Cincinnati,
+                  OH)*
+                </p>
+                <p>
+                  Pout, Present &amp; Tense, Let's Pretend Records (Bloomington,
+                  IN)*
+                </p>
+                <p>Marr, MARR, Independent Release (Cincinnati, OH)*</p>
+              </div>
+            </li>
+            <li class="flex-dir-row">
+              <div>2018</div>
+              <div class="some-margin-left">
+                <p>
+                  Santamania 2018, Independent Publication (Cincinnati, OH)*
+                </p>
+                <p>
+                  Canvas &amp; Cassette, Issue 3, Independent Publication (New
+                  York, NY)*
+                </p>
+              </div>
+            </li>
+            <li class="flex-dir-row">
+              <div>2016</div>
+              <div class="some-margin-left">
+                <p>
+                  CVSN: A Field Guide to Color, Special Collections Press
+                  (Cincinnati, OH)
+                </p>
+              </div>
+            </li>
+          </ul>
+          <h2>Press</h2>
+          <ul>
+            <li className="flex-dir-row">
+              <div>2022</div>
+              <div className="some-margin-left">
+                <p>Artswave- The Teachers Lounge- The Kennedy Gallery</p>
+              </div>
+            </li>
+            <li className="flex-dir-row">
+              <div></div>
+              <div className="some-margin-left">
+                <p>Happening Next- (Y)our Community- The Elavar Gallery</p>
+              </div>
+            </li>
+            <li className="flex-dir-row">
+              <div>2015</div>
+              <div className="some-margin-left">
+                <p>The News Record- Artist Feature- Cincinnati, OH</p>
+              </div>
+            </li>
+          </ul>
+          <h2>Discography</h2>
+          <ul>
+            <li>
+              <div>2014</div>
+              <div class="some-margin-left">
+                <p>
+                  Cateyes’ Assorted Sounds, Cateyes’ Assorted Sounds,
+                  Independent Release (Cincinnati, OH)
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <h2>Experience</h2>
+        <ul>
+          <li className="flex-dir-row">
+            <div>2022</div>
+            <div className="some-margin-left">
+              <p>Studio Assistant to Gee Horton (Cincinnati, OH)</p>
+              <p>Photo Assistant to Chris Cone (Cincinnati, OH)</p>
+              <p>Photo Assistant to Lance Adkins (Cincinnati, OH)</p>
+              <p>Photo Assistant Joshua Berg (Cincinnati, OH)</p>
+              <p>Photo Assistant Todd Joyce (Cincinnati, OH)</p>
+              <p>
+                Digital Print Specialist, Dodd Camera, 2016-2022 (Cincinnati,
+                OH)
+              </p>
+            </div>
+          </li>
+          <li className="flex-dir-row">
+            <div>2016</div>
+            <div className="some-margin-left">
+              <p>Studio Assistant to Jeanette Nuxol (Cincinnati, OH)</p>
+              <p>
+                Vice President, Student Artist Approaching Potential, DAAP
+                School of Art 2015-2016 (Cincinnati, OH)
+              </p>
+              <p>
+                Member, Student Artist Approaching Potential, DAAP School of Art
+                2013-2016 (Cincinnati, OH)
+              </p>
+            </div>
+          </li>
+          <li className="flex-dir-row">
+            <div>2014</div>
+            <div className="some-margin-left">
+              <p>Studio Assistant to Kate Ball (Cincinnati, OH)</p>
+            </div>
+          </li>
+        </ul>
+      </section>
+    </div>
   );
 };
 
